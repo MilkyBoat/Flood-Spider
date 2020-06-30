82b2063908f09fc4923f754faa239d0e48ad870e
milkyboat <1214208054@qq.com>
2020-07-01 02:54:14 +0800

    show data with web

---
+++ js/data0.js
@@ -0,0 +1,84 @@
+var level = {
+    backgroundColor: '#2c343c',
+    textStyle: {
+        color: 'rgba(255, 255, 255, 0.3)'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            data: [
+                { value: 400, name: '搜索引擎' },
+                { value: 335, name: '直接访问' },
+                { value: 310, name: '邮件营销' },
+                { value: 274, name: '联盟广告' },
+                { value: 235, name: '视频广告' }
+            ],
+            roseType: 'angle',
+
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+            label: {
+                normal: {
+                    textStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            }
+        }
+    ]
+}
+var flow = {
+    backgroundColor: '#2c343c',
+    textStyle: {
+        color: 'rgba(255, 255, 255, 0.3)'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            data: [
+                { value: 400, name: '搜索引擎' },
+                { value: 335, name: '直接访问' },
+                { value: 310, name: '邮件营销' },
+                { value: 274, name: '联盟广告' },
+                { value: 235, name: '视频广告' }
+            ],
+            roseType: 'angle',
+
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+            label: {
+                normal: {
+                    textStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            }
+        }
+    ]
+}
